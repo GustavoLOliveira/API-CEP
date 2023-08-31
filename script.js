221b4a05b75aa2aa54ac2ec9991e1447a602b269
Gustavo Lopes de Oliveira <84816069+GustavoLOliveira@users.noreply.github.com>
2023-08-31 20:15:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -25,4 +25,4 @@ async function buscaEndereco(cep){
 }
 
 var cep = document.getElementById('cep');
-cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+cep.addEventListener("focusout",{() => buscaEndereco(cep.value)});
